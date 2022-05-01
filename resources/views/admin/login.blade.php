9972601dbfe819a59a52b0c34556c8630507e4e2
Ajith D <ajithd78564@gmail.com>
2022-05-01 22:18:15 +0530

    updated

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -19,15 +19,15 @@
      <div class="row">
       <form action="{{ route('AdminLoginData') }}" method="POST">
         @csrf
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input name="password" type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
       </form>
      </div>
    </div>
